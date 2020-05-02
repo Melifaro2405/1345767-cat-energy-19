504d37d0a3a79e06e37f38fbc5d21c38af14b3bc
Melifaro2405 <tomelifaro@yandex.ru>
2020-05-02 18:08:16 +0300

    готовим проект к первой попытке защиты

--- gulpfile.js
+++ gulpfile.js
@@ -82,6 +82,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
+    "source/js/picturefill.min.js",
     "source/*.ico"
   ], {
     base: "source"
